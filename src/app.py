c74ea41fd1466e5864623b69be0b3b2b87c0e51d
Jesse schultz <wildstingray@gmail.com>
2020-02-03 22:13:35 -0600

    Make changes as learning :)

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import seaborn as sns
-import pandas as pd
+import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
 # from alpha_vantage.sectorperformance import SectorPerformances
 # import matplotlib.pyplot as plt
@@ -10,22 +10,17 @@ os.environ["ALPHAVANTAGE_API_KEY"] = 'GRTMSJRG1AXL8822'
 
 ts = TimeSeries(output_format='pandas')
 
-data, meta_data = ts.get_intraday(symbol='GOOGL', interval='1min',
-                                  outputsize='full')
+data, meta_data = ts.get_intraday(symbol='TSLA', interval='1min',
+                                  outputsize='compact')
 
 print(data)
 
 data['returns'] = np.log(data['4. close'] / data['4. close'].shift(1))
-
-if (pd.isnull(data['returns'])):
-    data['returns'] = data['returns'][~pd.isnull(data['returns'])]
-
-
 print(data['returns'])
 
 cols = []
 
-for momentum in [15, 30, 60, 120]:
+for momentum in [15, 30, 60]:
     col = 'position_%s' % momentum
     data[col] = np.sign(data['returns'].rolling(momentum).mean())
     cols.append(col)
@@ -38,7 +33,7 @@ for col in cols:
     strats.append(strat)
 
 data[strats].dropna().cumsum().apply(np.exp).plot()
-
+plt.show()
 # Trigger
 
 # Determine stoploss
@@ -46,3 +41,7 @@ data[strats].dropna().cumsum().apply(np.exp).plot()
 # Determine Price Target
 
 # Execute Trade
+
+
+# create simulation to replay historic data
+# test algorithms of buying and selling against this
